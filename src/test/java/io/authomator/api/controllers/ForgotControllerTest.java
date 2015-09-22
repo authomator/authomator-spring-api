3e9a554f1700f58bbdde374a6a9347135b41c124
Stefan Lapers <stefan@lapers.be>
2015-09-22 08:34:10 +0200

    add reset password testing on ForgotController

--- src/test/java/io/authomator/api/controllers/ForgotControllerTest.java
+++ src/test/java/io/authomator/api/controllers/ForgotControllerTest.java
@@ -7,6 +7,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jose4j.jws.JsonWebSignature;
+
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
 
@@ -23,13 +28,19 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.Assert;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.authomator.api.AuthomatorApiApplication;
 import io.authomator.api.builders.ForgotPasswordRequestBuilder;
 import io.authomator.api.builders.UserBuilder;
 import io.authomator.api.domain.entity.User;
 import io.authomator.api.domain.repository.UserRepository;
+import io.authomator.api.domain.service.UserService;
+import io.authomator.api.dto.TokenReply;
+import io.authomator.api.jwt.JwtService;
 import io.authomator.api.mail.MailService;
 import io.authomator.api.mail.MailTransport;
 
@@ -52,7 +63,13 @@ public class ForgotControllerTest {
 	private UserRepository userRepository;
 	
 	@Autowired
+	private UserService userService;
+	
+	@Autowired
 	private MailService mailService;
+	
+	@Autowired 
+	private JwtService jwtService;
 	
 	private MockMvc mockMvc;
 	
@@ -81,6 +98,13 @@ public class ForgotControllerTest {
     	userRepository.deleteAll();
     }
     
+    /*
+	  ___              _   ___                 _     __  __      _ _ 
+	 / __| ___ _ _  __| | | __|__ _ _ __ _ ___| |_  |  \/  |__ _(_) |
+	 \__ \/ -_) ' \/ _` | | _/ _ \ '_/ _` / _ \  _| | |\/| / _` | | |
+	 |___/\___|_||_\__,_| |_|\___/_| \__, \___/\__| |_|  |_\__,_|_|_|
+	                                 |___/                           
+    */
     
     @Test
     public void sendMailResetToken_sends_mail() throws Exception {
@@ -183,5 +207,109 @@ public class ForgotControllerTest {
     	verify(mockTransport, never()).sendForgotEmail(null, null);
     }
     
+    /*
+	  ___             _     ___                              _ 
+	 | _ \___ ___ ___| |_  | _ \__ _ _______ __ _____ _ _ __| |
+	 |   / -_|_-</ -_)  _| |  _/ _` (_-<_-< V  V / _ \ '_/ _` |
+	 |_|_\___/__/\___|\__| |_| \__,_/__/__/\_/\_/\___/_| \__,_|
+                                                            
+     */
+    
+    
+    @Test
+    public void reset_with_valid_token() throws Exception {
+
+    	User user = userRepository.findByEmail(USER_EMAIL);
+    	System.out.println(user.getPassword());
+    	Assert.notNull(user);
+    	
+    	JsonWebSignature token = jwtService.getForgotPasswordToken(user);
+    	
+    	Map<String, String> req = new HashMap<>();
+    	req.put("password", "newpassword");
+    	
+    	mockMvc
+    		.perform(
+				post("/api/auth/forgot/" + token.getCompactSerialization())
+				.accept(APPLICATION_JSON)
+				.contentType(APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(req))
+			)
+    		.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(APPLICATION_JSON))
+            .andExpect(jsonPath("$.at").exists())
+            .andExpect(jsonPath("$.rt").exists())
+    		.andExpect(jsonPath("$.it").exists());
+    	
+    	User newPassUser = userService.login(USER_EMAIL, "newpassword");
+    	Assert.notNull(newPassUser);
+
+    }
+    
+
+    @Test
+    public void reset_with_valid_token_but_nonexisting_user() throws Exception {
+
+    	User user = userRepository.findByEmail(USER_EMAIL);
+    	Assert.notNull(user);
+    	
+    	JsonWebSignature token = jwtService.getForgotPasswordToken(user);
+    	
+    	userRepository.deleteAll();
+    	
+    	Map<String, String> req = new HashMap<>();
+    	req.put("password", "newpassword");
+    	
+    	mockMvc
+    		.perform(
+				post("/api/auth/forgot/" + token.getCompactSerialization())
+				.accept(APPLICATION_JSON)
+				.contentType(APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(req))
+			)
+    		.andDo(print())
+            .andExpect(status().isUnprocessableEntity())
+            .andExpect(content().contentType(APPLICATION_JSON))
+            .andExpect(jsonPath("$.message").value("Validation Failed"))
+	        .andExpect(jsonPath("$.code").value("ValidationFailed"))
+			.andExpect(jsonPath("$.fieldErrors").isArray())
+			.andExpect(jsonPath("$.fieldErrors", hasSize(1)))
+			.andExpect(jsonPath("$.fieldErrors[0].field").value("email"))
+	        .andExpect(jsonPath("$.fieldErrors[0].message").value("Invalid or non existing email"))
+	        .andExpect(jsonPath("$.fieldErrors[0].code").value("CredentialsError"));
+    }
+    
+    @Test
+    public void reset_with_invalid_token() throws Exception {
+
+    	User user = userRepository.findByEmail(USER_EMAIL);
+    	Assert.notNull(user);
+    	
+    	JsonWebSignature token = jwtService.getForgotPasswordToken(user);
+    	
+    	userRepository.deleteAll();
+    	
+    	Map<String, String> req = new HashMap<>();
+    	req.put("password", "newpassword");
+    	
+    	mockMvc
+    		.perform(
+				post("/api/auth/forgot/" + token.getCompactSerialization() + "defect")
+				.accept(APPLICATION_JSON)
+				.contentType(APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(req))
+			)
+    		.andDo(print())
+            .andExpect(status().isUnprocessableEntity())
+            .andExpect(content().contentType(APPLICATION_JSON))
+            .andExpect(jsonPath("$.message").value("Validation Failed"))
+	        .andExpect(jsonPath("$.code").value("ValidationFailed"))
+			.andExpect(jsonPath("$.fieldErrors").isArray())
+			.andExpect(jsonPath("$.fieldErrors", hasSize(1)))
+			.andExpect(jsonPath("$.fieldErrors[0].field").value("token"))
+	        .andExpect(jsonPath("$.fieldErrors[0].message").value("Invalid jwt token"))
+	        .andExpect(jsonPath("$.fieldErrors[0].code").value("InvalidToken"));
+    }
 	
 }
